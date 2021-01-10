68df1909f1663d4f29f0321d2e4d312f317b78f5
Raffaele Marcello <marcelloraffaele@gmail.com>
2021-01-10 20:44:04 +0100

    improvements

--- docker/dbinit/2_dml.sql
+++ docker/dbinit/2_dml.sql
@@ -1,5 +1,9 @@
 USE eventdb;
 
-INSERT INTO `event` (`id`, `title`, `artist`, `description`, `start_date`, `location`, `address`, `price`, `availability`, `img`) VALUES
-(1,	't1',	'a1',	'd1',	'2021-01-01 19:41:39',	'w1',	'a1',	123.45,	12345,	'something/aaa.jpg'),
-(2,	't2',	'a2',	'd2',	'2021-01-01 19:42:22',	'w2',	'a2',	678.9,	123455,	'something/aaa.jpg');
+INSERT INTO `event` ( `title`, `artist`, `description`, `start_date`, `location`, `address`, `price`, `availability`, `img`) VALUES
+('Foo Fighters',	'Foo Fighters',	'Riconfermati i Foo Fghters nell ambito di I-Days Milano 2021. Scopri i dettagli!',	'2021-06-12 17:00:00',	'Milano',	'I-DAYS',	74.50,	1000,	'img/ff.jpg'),
+('Deep Purple',	'Deep Purple THE WHOOSH! TOUR',	'Deep Purple THE WHOOSH! TOUR',	'2021-07-06 21:00:00',	'w2',	'a2',	57.50,	1000,	'img/dp.jpg'),
+('James Blunt Once upon a tour',	'James Blunt',	'James Blunt ONCE UPON A MIND TOUR',	'2021-06-27 21:15:00',	'Milano',	'Mediolanum forum',	80.50,	700,	'img/jb.jpg'),
+('Vasco Non stop live festival',	'Vasco Rossi',	'Vasco Non stop live festival',	'2021-06-27 21:15:00',	'Firenze',	'Visarno Arena',	80.50,	700,	'img/vr.jpg'),
+('Hans Zimmer',	'Hans Zimmer',	'd2',	'2022-03-30 19:42:22',	'ASSAGO',	'Mediolanum Forum',	103.50,	1000,	'img/hz.jpg'),
+('Carmen Consoli - Tour 2021',	'Carmen Consoli',	'Carmen Consoli torna con un nuovo tour nel 2021! Acquista subito il tuo biglietto!',	'2021-12-26 21:00:00',	'Roma',	'Auditorium Parco della Musica - Sala Santa Cecilia',	46,	1000,	'img/cc.jpg');
